3042d2c9434310ec6369fbb4aca9cfed7078b359
genshibangou16 <genshibangou16.rakkan@gmail.com>
2025-04-22 18:42:05 +0900

    メモアプリにメモの表示機能を実装し、レイアウトとスタイルを改善

--- app.rb
+++ app.rb
@@ -2,7 +2,34 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
+
+before do
+  file = File.read('./public/memo.json')
+  parsed = JSON.parse(file)
+  @memos = parsed.transform_values do |memo|
+    memo.transform_keys(&:to_sym)
+  end
+end
 
 get '/' do
+  @css = 'index.css'
   erb :index
+end
+
+get '/add' do
+  erb :add
+end
+
+get '/:memo_id' do
+  memo_id = params[:memo_id]
+
+  if @memos.key?(memo_id)
+    @css = 'show.css'
+    @memo = @memos[memo_id]
+    erb :show
+  else
+    status 404
+    erb :not_found
+  end
 end
