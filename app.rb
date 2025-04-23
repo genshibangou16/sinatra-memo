c92ffc11987a55f6298af0f348651edfb0fad5f2
genshibangou16 <genshibangou16.rakkan@gmail.com>
2025-04-23 14:21:01 +0900

    メモの作成、編集、削除機能を追加し、エラーハンドリングを強化。フォームに必須項目を設定し、JSONファイルへの保存処理を共通化。

--- app.rb
+++ app.rb
@@ -23,6 +23,12 @@ helpers do
   def h(text)
     Rack::Utils.escape_html(text)
   end
+
+  def save_as_json(hash)
+    File.open(MEMOS_FILE_PATH, 'w') do |f|
+      JSON.dump(hash, f)
+    end
+  end
 end
 
 get '/' do
@@ -36,13 +42,13 @@ get '/memos' do
 end
 
 post '/memos' do
+  redirect '/error' if params[:title].empty?
+
   uuid = SecureRandom.uuid
-  @title = params[:title]
-  @content = params[:content]
-  @memos[uuid] = { title: @title, timestamp: DateTime.now.iso8601, content: @content }
-  File.open(MEMOS_FILE_PATH, 'w') do |f|
-    JSON.dump(@memos, f)
-  end
+  title = params[:title]
+  content = params[:content]
+  @memos[uuid] = { title: title, timestamp: DateTime.now.iso8601, content: content }
+  save_as_json(@memos)
   redirect "/memos/#{uuid}"
 end
 
@@ -62,12 +68,63 @@ get '/memos/:memo_id' do
   if @memos.key?(memo_id)
     @css = 'show.css'
     @memo = @memos[memo_id]
-    @additional_button = { href: '/memos', text: '戻る' }
+    @memo[:id] = memo_id
+    @additional_button = { href: '/memos', text: 'ホーム' }
     erb :show
   else
     status 404
     erb :not_found
   end
+end
+
+delete '/memos/:memo_id' do
+  memo_id = params[:memo_id]
+
+  if @memos.key?(memo_id)
+    @memos.delete(memo_id)
+    save_as_json(@memos)
+    redirect '/memos'
+  else
+    status 404
+    erb :not_found
+  end
+end
+
+patch '/memos/:memo_id' do
+  memo_id = params[:memo_id]
+
+  if @memos.key?(memo_id)
+    redirect '/error' if params[:title].empty?
+
+    title = params[:title]
+    content = params[:content]
+    @memos[memo_id] = { title: title, timestamp: DateTime.now.iso8601, content: content }
+    save_as_json(@memos)
+    redirect "/memos/#{memo_id}"
+  else
+    status 404
+    erb :not_found
+  end
+end
+
+get '/memos/:memo_id/edit' do
+  memo_id = params[:memo_id]
+
+  if @memos.key?(memo_id)
+    @css = 'new.css'
+    @memo = @memos[memo_id]
+    @memo[:id] = memo_id
+    @additional_button = { href: "/memos/#{memo_id}", text: '戻る' }
+    erb :new
+  else
+    status 404
+    erb :not_found
+  end
+end
+
+get '/error' do
+  @additional_button = { href: '/memos', text: 'ホーム' }
+  erb :error
 end
 
 not_found do
